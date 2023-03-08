58e57b491bcf15316cae7c9e8948217d38241eb6
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-03-08 21:28:32 +0100

    Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId;
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
+
+function start() {
+  if (intervalId) return;
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+
+  startButton.disabled = true;
+}
+function stop() {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+  intervalId = null;
+}
+
+console.log(startButton);
+console.log(stopButton);
